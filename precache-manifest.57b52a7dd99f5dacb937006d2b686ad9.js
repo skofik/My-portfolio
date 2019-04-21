35ee42bf94e02ea846fc0c543fe1e53ebdbe3a5a
Daniel Skowron <skofikit@gmail.com>
2019-04-21 15:13:56 +0200

    Updates

---
+++ precache-manifest.57b52a7dd99f5dacb937006d2b686ad9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d12727417021fec07661bd65ec212e6d",
+    "url": "/Gallery-3D/static/media/Calorie-counter665x307.d1272741.jpg"
+  },
+  {
+    "revision": "2e5f7037118d555bd668",
+    "url": "/Gallery-3D/static/css/main.7374f1b5.chunk.css"
+  },
+  {
+    "revision": "41f84ea320a19e4e9fc2",
+    "url": "/Gallery-3D/static/js/runtime~main.56eb9bbd.js"
+  },
+  {
+    "revision": "4843e353bd758d5d208d",
+    "url": "/Gallery-3D/static/js/2.48176a17.chunk.js"
+  },
+  {
+    "revision": "399380f0bdd8375b28d17723557bddf4",
+    "url": "/Gallery-3D/static/media/Project-Tic-tac-toe640x343.399380f0.jpg"
+  },
+  {
+    "revision": "2e5f7037118d555bd668",
+    "url": "/Gallery-3D/static/js/main.9921a5e9.chunk.js"
+  },
+  {
+    "revision": "7d40b6bbf0335ed2fcc229435041f060",
+    "url": "/Gallery-3D/static/media/Gallery3D593x304.7d40b6bb.jpg"
+  },
+  {
+    "revision": "553331c3e83e7e67bcdc23b0972a8488",
+    "url": "/Gallery-3D/static/media/ladyIgy661x314.553331c3.jpg"
+  },
+  {
+    "revision": "e323c80a7fd1fe0680869dfa78a1a56f",
+    "url": "/Gallery-3D/static/media/Portfolio681x311.e323c80a.jpg"
+  },
+  {
+    "revision": "77859be09a57fb123bf9c3d200cc893d",
+    "url": "/Gallery-3D/static/media/tlo.77859be0.svg"
+  },
+  {
+    "revision": "774ee298ef403dfce8054283da161605",
+    "url": "/Gallery-3D/index.html"
+  }
+];
